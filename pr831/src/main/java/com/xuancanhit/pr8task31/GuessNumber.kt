88768c3dd814e70c47994668358ea97047e63bfb
Xuan Canh <xuancanhit99@gmail.com>
2022-04-22 06:57:34 +0300

    up

---
+++ pr831/src/main/java/com/xuancanhit/pr8task31/GuessNumber.kt
@@ -0,0 +1,11 @@
+package com.xuancanhit.pr8task31
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuessNumber : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guess_number)
+    }
+}
